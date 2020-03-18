6e709c502a48b78199f8285076af46ed3fe5031f
Martin Zikmund <martin@zikmund.dev>
2020-03-18 13:40:44 +0100

    Added WanaKanaOptions class, further project structuring

---
+++ src/WanaKana/Checkers/HiraganaChecker.cs
@@ -0,0 +1,10 @@
+﻿namespace WanaKanaNet.Checkers
+{
+    internal static class HiraganaChecker
+    {
+        public static bool IsHiragana(string input)
+        {
+            return false;
+        }
+    }
+}
---
+++ src/WanaKana/Enums/ImeMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WanaKanaNet.Enums
+{
+    /// <summary>
+    /// Represents available IME modes.
+    /// </summary>
+    public enum ImeMode
+    {
+        None,
+        Enabled,
+        ToHiragana,
+        ToKatakana
+    }
+}
