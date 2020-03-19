80b4656277e1af92bd21d0aad5b3178f2a29272d
Martin Zikmund <martin@zikmund.dev>
2020-03-19 10:44:03 +0100

    test: Japanese and English punctuation, Vowels and Consonants

--- tests/WanaKana.Tests/Checkers/SpecialCharacterCheckerTests.cs
+++ tests/WanaKana.Tests/Checkers/SpecialCharacterCheckerTests.cs
@@ -17,6 +17,11 @@ namespace WanaKanaNet.Tests.Checkers
             Assert.False(SpecialCharacterChecker.IsConsonant('y', false));
         }
 
+        [Fact]
+        public void IsConsonantIncludesYByDefault()
+        {
+            Assert.True(SpecialCharacterChecker.IsConsonant('y'));
+        }
 
         [Fact]
         public void IsConsonantRecognizesConsonants()
@@ -28,11 +33,103 @@ namespace WanaKanaNet.Tests.Checkers
         }
 
         [InlineData('a')]
-        [InlineData('!')]        
+        [InlineData('!')]
         [Theory]
         public void IsConsonantInvalidCases(char character)
         {
             Assert.False(SpecialCharacterChecker.IsConsonant(character));
+        }
+
+        [Fact]
+        public void IsVowelExcludingY()
+        {
+            Assert.False(SpecialCharacterChecker.IsVowel('y', false));
+        }
+
+        [Fact]
+        public void IsVowelRecognizesVowels()
+        {
+            foreach (var vowel in CharacterBounds.Vowels)
+            {
+                Assert.True(SpecialCharacterChecker.IsVowel(vowel));
+            }
+        }
+
+        [InlineData('x')]
+        [InlineData('!')]
+        [Theory]
+        public void IsVowelInvalidCases(char character)
+        {
+            Assert.False(SpecialCharacterChecker.IsVowel(character));
+        }
+
+        [Fact]
+        public void IsEnglishPunctuationAllEnglishPunctuationsAreRecognized()
+        {
+            foreach (var range in CharacterBounds.EnglishPunctuationRanges)
+            {
+                for (char character = range.Start; character <= range.End; character++)
+                {
+                    Assert.True(SpecialCharacterChecker.IsEnglishPunctuation(character));
+                }
+            }
+        }
+
+        [Fact]
+        public void IsEnglishPunctuationAllJapanesePunctuationsAreInvalid()
+        {
+            foreach (var range in CharacterBounds.JapanesePunctuationRanges)
+            {
+                for (char character = range.Start; character <= range.End; character++)
+                {
+                    Assert.False(SpecialCharacterChecker.IsEnglishPunctuation(character));
+                }
+            }
+        }
+
+        [InlineData(' ', true)]
+        [InlineData('a', false)]
+        [InlineData('ふ', false)]
+        [InlineData('字', false)]
+        [Theory]
+        public void IsEnglishPunctuationVarious(char character, bool expectedResult)
+        {
+            Assert.Equal(expectedResult, SpecialCharacterChecker.IsEnglishPunctuation(character));
+        }
+
+        [Fact]
+        public void IsJapanesePunctuationAllJapanesePunctuationsAreRecognized()
+        {
+            foreach (var range in CharacterBounds.JapanesePunctuationRanges)
+            {
+                for (char character = range.Start; character <= range.End; character++)
+                {
+                    Assert.True(SpecialCharacterChecker.IsJapanesePunctuation(character));
+                }
+            }
+        }
+
+        [Fact]
+        public void IsJapanesePunctuationAllEnglishPunctuationsAreInvalid()
+        {
+            foreach (var range in CharacterBounds.EnglishPunctuationRanges)
+            {
+                for (char character = range.Start; character <= range.End; character++)
+                {
+                    Assert.False(SpecialCharacterChecker.IsJapanesePunctuation(character));
+                }
+            }
+        }
+
+        [InlineData('　', true)]
+        [InlineData('?', false)]
+        [InlineData('a', false)]
+        [InlineData('ふ', false)]
+        [InlineData('字', false)]
+        [Theory]
+        public void IsJapanesePunctuationVarious(char character, bool expectedResult)
+        {
+            Assert.Equal(expectedResult, SpecialCharacterChecker.IsJapanesePunctuation(character));
         }
     }
 }
